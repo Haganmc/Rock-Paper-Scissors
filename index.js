a0bd8f062d1b371d445b1faeafa22cb64993d537
Haganmc <hagan_mc@outlook.com>
2025-03-31 09:54:41 -0500

    play rock paper scissors

--- index.js
+++ index.js
@@ -12,15 +12,59 @@ function getComputerChoice () {
     }
 }
 
-function getUserChoice() {
+function getHumanChoice() {
     // Ask for user's choice
     // Get user choice and store it in variable
-    const user_choice = prompt(
-        "Enter your choice: Rock, Paper, or Scissors").toLowerCase();
+    const humanChoice = prompt(
+        "Enter your choice: Rock, Paper, or Scissors");
     // Return the users choice
-    return user_choice
+    return humanChoice
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You Win! Rock beats Scissors");
+        humanScore += 1;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You Lose! Paper beats Rock");
+        computerScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You Win! Paper beats rock");
+        humanScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You Lose! Scissors beats paper");
+        computerScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You Win! Scissors beats Paper");
+        humanScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You Lose! Rock beats Scissors");
+        computerScore += 1;
+    } else {
+        console.log("TIE!");
+    }
+}
+
+function playGame(){
+    for (let i = 1; i < 6; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (humanScore > computerScore){
+        console.log("You Win!!");
+    } else if (humanScore < computerScore) {
+        console.log("Sorry you lose! Try Again!");
+    } else {
+        console.log("Tie")
+    }
 }
 
 // initialize scores
 let computerScore = 0;
 let humanScore = 0;
+
+playGame();
