01868afde93d2b9b69e664233474c91252c8e04f
Haganmc <hagan_mc@outlook.com>
2025-03-31 09:22:33 -0500

    added computer's choice

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function getComputerChoice () {
+    choice = Math.floor(Math.random() * 3);
+    console.log(choice);
+    switch(choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
